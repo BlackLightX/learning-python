a35f19eaf811eb3d4aa57d38a1c61e066c71457e
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-11 17:46:40 +0300

    Add files via upload

---
+++ projeler/data-types-test.py
@@ -0,0 +1,20 @@
+"""
+pi = float(3.14)
+
+r = float(input("Yariçapı giriniz: "))
+
+daireAlani = pi * (r ** 2)
+
+daireCevresi = 2 * pi *  r
+
+print("Dairenin alanı: ",daireAlani)
+print("Dairenin çevresi: ",daireCevresi)
+"""
+
+km = float(input("Gidilen km'yi giriniz: "))
+
+mil = km / 1.609344
+
+print("Gidilen mil: ",mil)
+
+print("Gidilen mil: ",round(mil,2))
