e834a985e296abaff34770e84b2ccfbb6ec02abd
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-11 15:39:37 +0300

    Update and rename code.py to variable_test.py

---
+++ variable_test.py
@@ -0,0 +1,19 @@
+ogrenciAdi = "Ahmet"
+ogrenciSoyadi = "Yılmaz"
+ogrenciAdveSoyad = ogrenciAdi + " " + ogrenciSoyadi
+print(ogrenciAdveSoyad)
+
+ogrenciNo = "530"
+ogrenciCinsiyet = "Erkek"
+ogrenciTCKimlik = "12345678911"
+ogrenciAdresiBilgisi = "İstanbul"
+
+ogrenciDogumYili = 2000
+suankiYil = 2024
+ogrenciYasi = suankiYil - ogrenciDogumYili
+print(ogrenciYasi)
+
+
+urun1, urun2, urun3 = 50, 70.3, 317.84
+toplam =  urun1 + urun2 + urun3
+print("Ürün toplamı", toplam)
