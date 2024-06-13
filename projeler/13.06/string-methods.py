0c0fad115c7e0c1694f5bb2601587863f36b9a61
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-13 22:18:52 +0300

    Add files via upload

---
+++ projeler/13.06/string-methods.py
@@ -0,0 +1,26 @@
+
+website = "https://www.sadikturan.com"
+kursAdi = "Python Dersleri: Sıfırdan İleri Seviye Python Programlama."
+
+"""
+dene = ' Hello World '.rstrip().lstrip()
+
+print(dene)
+"""
+
+sonuc = kursAdi.lower()
+
+print(sonuc)
+
+deneme = website.count('a')
+
+print(deneme)
+
+deneme2 = "Selam".center(40,"1")
+
+print(deneme2)
+
+deneme3 = website.find("ttps")
+
+print(deneme3)
+
