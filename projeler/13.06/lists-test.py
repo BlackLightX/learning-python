7540318446db1c2a9c033add44458d541dcc7d98
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-13 23:24:51 +0300

    Add files via upload

---
+++ projeler/13.06/lists-test.py
@@ -0,0 +1,19 @@
+deneme = ["Samsung S5" ,"Samsung S6", "Samsung S7", "Samsung S8"]
+
+print(deneme)
+
+sonuc1 = len(deneme)
+
+print(sonuc1)
+
+sonuc2 = deneme[0]
+sonuc2 = deneme[-1]
+
+print(sonuc2)
+
+sonuc3 = deneme[0:2]
+print(sonuc3)
+
+deneme[-2:] = ["Samsung S9","Samsung S10"]
+
+print(deneme)
