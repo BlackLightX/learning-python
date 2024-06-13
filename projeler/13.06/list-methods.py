0db4328c64f448b12102fe915df590a55e90f10e
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-14 00:07:58 +0300

    Add files via upload

---
+++ projeler/13.06/list-methods.py
@@ -0,0 +1,28 @@
+isimler = ['Ada', 'Yiğit', 'Hasan', 'Beril']
+yaslar = [1998, 2000, 1998, 1987]
+
+"""
+isimler.append("Cenk")
+print(isimler)
+
+"""
+"""
+
+isimler.insert(0,"Sena")
+print(isimler)
+
+"""
+"""
+
+isimler.pop(1)
+print(isimler)
+
+"""
+"""
+
+isimler.remove("Yiğit")
+print(isimler)
+"""
+
+indexNo = isimler.index("Yiğit")
+print(indexNo)
