b854ac8df54ec27e50f576ccb13d5f91ef748a8e
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-15 14:53:10 +0300

    Create atama-op.py

---
+++ projeler/15.06/atama-op.py
@@ -0,0 +1,39 @@
+a, b, c = 2, 5, 12
+
+# x, y = int(input("x: ")), int(input("y: "))
+
+# carpim = x*y
+
+# toplam = a + b + c
+
+# print(carpim-toplam)
+
+
+# sonuc = c//b
+# print(sonuc)
+
+# sonuc = (a+b+c) % 3
+# print(sonuc)
+
+# sonuc = b**a
+# print(sonuc)
+
+# sayilar = 1, 5, 7, 10, 3
+
+# a, *b, c = sayilar
+
+# sonuc = c**3
+
+# print(sonuc)
+
+
+sayilar = 1, 5, 7, 10, 3
+
+a, *b, c = sayilar
+
+print(a, b, c)
+print(sayilar)
+
+b_sayilar = b[0] + b[1] + b[2]
+
+print(b_sayilar)
