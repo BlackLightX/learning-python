509839cb52971d82d7a31ce13763247a12c09b1f
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-06-19 15:56:19 +0300

    Add files via upload

---
+++ projeler/19.06/for-while.py
@@ -0,0 +1,28 @@
+sayilar = [1,5,15,35,57,72,75]
+
+# for sayi in sayilar:
+#     print(sayi)
+
+# for sayi in sayilar:
+#     if (sayi % 5 == 0):
+#         print(sayi)
+
+
+# toplam=0
+# for sayi in sayilar:
+#     toplam = toplam + sayi
+#     print(toplam)
+
+# print(toplam)
+
+
+urunler = ['iphone 8','iphone X','iphone XR','samsung S10']
+
+for urun in urunler:
+    print(urun[1])
+
+
+for urun in urunler:
+    print(urun.find('iphone'))
+
+
