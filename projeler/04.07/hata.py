5f8200358755d786cb929a97143df34d662a683a
BlackLight <65299525+BlackLightX@users.noreply.github.com>
2024-07-04 16:05:18 +0300

    Create hata.py

---
+++ projeler/04.07/hata.py
@@ -0,0 +1,38 @@
+# try:
+#     x = int(input("x= "))
+#     y = int(input("y= "))
+#     print("x + y =", x + y)
+# except:
+#     print("Hata oluştu.")
+
+####################################
+
+# SyntaxError ==> yazım yanlışı
+# def yazdir((:
+#     pass
+    
+
+# NameError ==> tanımlanmamış bir değişken kullanımı
+# print(isim)
+
+
+# TypeError ==> hatalı parametre kullanımı
+#len(10)  # str veya list olması gerekiyor ama biz int gönderdik
+
+
+# IndexError ==> yanlış indeks numarası
+# liste = ["Selam"]
+# liste[3]
+
+
+# ValueError ==> hatalı tip kullanımı
+# int("5a")
+
+
+# KeyError ==> key hatası
+# a = {}
+# a["ad"]
+
+
+# AttributeError ==> olmayan bir özelliğe ulaşmak istediğimizde
+# "selam".uppercase()
